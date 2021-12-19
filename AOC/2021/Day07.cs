e0fa42eb7d6ff3b52bd685748ba3cd248e002584
exirpit <jayveedees@gmail.com>
2021-12-19 02:07:09 +0000

    day07 complete

---
+++ AOC/2021/Day07.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC._2021
+{
+    public class Day07
+    {
+        public int PartOne(string[] input)
+        {
+            var crabs = Array.ConvertAll(input[0].Split(","), int.Parse);
+            var median = GetMedian(crabs);
+
+            var fuelSpent = 0;
+            foreach (var crab in crabs)
+            {
+                if (crab != median)
+                {
+                    var diff = median >= crab ? median - crab : crab - median;
+                    fuelSpent += diff;
+                }
+            }
+            
+            return fuelSpent;
+        }
+
+        public int PartTwo(string[] input)
+        {
+            var crabs = Array.ConvertAll(input[0].Split(","), int.Parse);
+            var mean = GetMean(crabs);
+
+            var fuelSpent = 0;
+            foreach (var crab in crabs)
+            {
+                if (crab != mean)
+                {
+                    var diff = mean >= crab ? mean - crab : crab - mean;
+                    for (int i = 1; i < diff + 1; i++)
+                    {
+                        fuelSpent += i;
+                    }
+                }
+            }
+
+            return fuelSpent;
+        }
+
+        private int GetMedian(int[] crabs)
+        {
+            var crabsLength = crabs.Length;
+            
+            Array.Sort(crabs);
+            
+            if (crabsLength % 2 != 0)
+            {
+                return crabs[crabsLength / 2];
+            }
+
+            return (crabs[(crabsLength - 1) / 2] + crabs[crabsLength / 2]) / 2;
+        }
+
+        private int GetMean(int[] crabs)
+        {
+            return Convert.ToInt32(Math.Round(crabs.Average())) - 1;
+        }
+    }
+}
---
+++ AOCTests/2021/Day07Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using AOC;
+using AOC._2021;
+using AOC.InputService;
+using NUnit.Framework;
+
+namespace AOC2021Tests._2021
+{
+    public class Day07Tests : IAOCTests
+    {
+        private const int Day = 7, PartOne = 1, PartTwo = 2;
+        private readonly IInputHandler _inputHandler = new InputHandler();
+        
+        [Test]
+        public void PartOneExample()
+        {
+            // Arrange
+            var unit = new Day07();
+            
+            // Act
+            var actual = unit.PartOne(_inputHandler.GetInput(Day, PartOne, true));
+            
+            // Assert
+            Assert.AreEqual(37, actual);
+            
+        }
+
+        [Test]
+        public void PartOneActual()
+        {
+            // Arrange
+            var unit = new Day07();
+            
+            // Act
+            var actual = unit.PartOne(_inputHandler.GetInput(Day, PartOne, false));
+            
+            // Assert
+            Console.WriteLine(actual);
+        }
+
+        [Test]
+        public void PartTwoExample()
+        {
+            // Arrange
+            var unit = new Day07();
+            
+            // Act
+            var actual = unit.PartTwo(_inputHandler.GetInput(Day, PartTwo, true));
+            
+            // Assert
+            Assert.AreEqual(168, actual);
+        }
+
+        [Test]
+        public void PartTwoActual()
+        {
+            // Arrange
+            var unit = new Day07();
+            
+            // Act
+            var actual = unit.PartTwo(_inputHandler.GetInput(Day, PartTwo, false));
+            
+            // Assert
+            Console.WriteLine(actual);
+            Assert.AreEqual(96708205, actual);
+            
+        }
+    }
+}
