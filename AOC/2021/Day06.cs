3397f52656dee95e844f596fa07d855b3a29127a
exirpit <jayveedees@gmail.com>
2021-12-19 00:32:43 +0000

    Day06 done

---
+++ AOC/2021/Day06.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using AOC._2021.HelperService;
+
+namespace AOC._2021
+{
+    public class Day06
+    {
+        public long PartOneTwo(string[] input, int days)
+        {
+            var fishies = new long[9];
+            foreach (int x in input[0].Split(',').Select(long.Parse))
+            {
+                fishies[x] += 1;
+            }
+            for (int _ = 0; _ < days; _++)
+            {
+                var fish0 = fishies[0];
+                for (int j = 0; j < 8; j++)
+                {
+                    fishies[j] = fishies[j + 1];
+                }
+                fishies[6] += fish0;
+                fishies[8] = fish0;
+            }
+
+            return fishies.Sum();
+        }
+    }
+}
---
+++ AOCTests/2021/Day06Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using AOC;
+using AOC._2021;
+using AOC.InputService;
+using NUnit.Framework;
+
+namespace AOC2021Tests._2021
+{
+    public class Day06Tests : IAOCTests
+    {
+        private const int Day = 6, PartOne = 1, PartTwo = 2;
+        private readonly IInputHandler _inputHandler = new InputHandler();
+        
+        [Test]
+        public void PartOneExample()
+        {
+            // Arrange
+            var unit = new Day06();
+
+            // Act
+            var actual1 = unit.PartOneTwo(_inputHandler.GetInput(Day, PartOne, true), 18);
+            var actual2 = unit.PartOneTwo(_inputHandler.GetInput(Day, PartOne, true), 80);
+
+            // Assert
+            Assert.AreEqual(26, actual1);
+            Assert.AreEqual(5934, actual2);
+
+        }
+
+        [Test]
+        public void PartOneActual()
+        {
+            // Arrange
+            var unit = new Day06();
+            
+            // Act
+            var actual = unit.PartOneTwo(_inputHandler.GetInput(Day, PartOne, false), 80);
+
+            // Assert
+            Console.WriteLine(actual);
+
+        }
+
+        [Test]
+        public void PartTwoExample()
+        {
+            // Arrange
+            var unit = new Day06();
+            
+            // Act
+            var actual = unit.PartOneTwo(_inputHandler.GetInput(Day, PartTwo, true), 256);
+            
+            // Assert
+            Assert.AreEqual(26984457539, actual);
+        }
+
+        [Test]
+        public void PartTwoActual()
+        {
+            // Arrange
+            var unit = new Day06();
+            
+            // Act
+            var actual = unit.PartOneTwo(_inputHandler.GetInput(Day, PartTwo, false), 256);
+            
+            // Assert
+            Console.WriteLine(actual);
+            Assert.AreEqual(1590327954513, actual);
+            
+        }
+    }
+}
