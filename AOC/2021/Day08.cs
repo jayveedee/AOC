085c87ca88672554b1018ab74d612a0eb58401ee
exirpit <jayveedees@gmail.com>
2021-12-19 03:41:51 +0000

    started day08

---
+++ AOC/2021/Day08.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace AOC._2021
+{
+    public class Day08
+    {
+        public int PartOne(string[] input)
+        {
+            var patterns = new int[4] {2, 3, 4, 7};
+
+            var counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                counter += input[i].Split("|")[1].Split().Sum(segment => patterns.Count(pattern => pattern == segment.Length));
+            }
+
+            return counter;
+        }
+
+        public int PartTwo(string[] input)
+        {
+            var decodedSegments = new DecodedSegment[9];
+            while (true)
+            {
+                foreach (var segmentLine in input)
+                {
+                    var encodedLines = segmentLine.Split("|")[0];
+                    for (int j = 0; j < encodedLines.Length; j++)
+                    {
+                        DecodedSegment? segment = null;
+                        var encodedSegment = encodedLines.Split()[j];
+                        switch (Convert.ToInt32(encodedSegment))
+                        {
+                            case 2:
+                                segment = new DecodedSegment(encodedSegment, 2);
+                                decodedSegments[2] = segment;
+                                break;
+                            case 3:
+                                segment = new DecodedSegment(encodedSegment, 3);
+                                decodedSegments[3] = segment;
+                                break;
+                            case 4:
+                                segment = new DecodedSegment(encodedSegment, 4);
+                                decodedSegments[4] = segment;
+                                break;
+                            case 7:
+                                segment = new DecodedSegment(encodedSegment, 7);
+                                decodedSegments[7] = segment;
+                                break;
+                        }
+
+                        if (segment != null)
+                        {
+                            break;
+                        }
+                        
+                    }
+                }
+            }
+            
+            return 0;
+        }
+        
+        public class DecodedSegment
+        {
+            private readonly string _name;
+            private readonly int _type;
+
+            public DecodedSegment(string name, int type)
+            {
+                _name = name;
+                _type = type;
+            }
+
+            public string Name => _name;
+
+            public int Type => _type;
+        }
+    }
+}
---
+++ AOCTests/2021/Day08Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AOC;
+using AOC._2021;
+using AOC.InputService;
+using NUnit.Framework;
+
+namespace AOC2021Tests._2021
+{
+    public class Day08Tests : IAOCTests
+    {
+        private const int Day = 8, PartOne = 1, PartTwo = 2;
+        private readonly IInputHandler _inputHandler = new InputHandler();
+        
+        [Test]
+        public void PartOneExample()
+        {
+            // Arrange
+            var unit = new Day08();
+
+            // Act
+            var actual = unit.PartOne(_inputHandler.GetInput(Day, PartOne, true));
+
+            // Assert
+            Assert.AreEqual(26, actual);
+
+        }
+
+        [Test]
+        public void PartOneActual()
+        {
+            // Arrange
+            var unit = new Day08();
+            
+            // Act
+            var actual = unit.PartOne(_inputHandler.GetInput(Day, PartOne, false));
+            
+            // Assert
+            Console.WriteLine(actual);
+        }
+
+        [Test]
+        public void PartTwoExample()
+        {
+            // Arrange
+            var unit = new Day08();
+
+            // Act
+            var actual = unit.PartTwo(_inputHandler.GetInput(Day, PartTwo, true));
+
+            // Assert
+            Assert.AreEqual(5353, actual);
+
+        }
+
+        [Test]
+        public void PartTwoActual()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
