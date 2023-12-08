65e59e6bd0a7324c3d516d638285c41147bb0bd0
fo0391 <jvd@formula.fo>
2023-12-08 15:25:41 +0000

    some renaming and started new aoc

---
+++ AOC/2023/D1.cs
@@ -0,0 +1,16 @@
+﻿using src.utils.interfaces;
+
+namespace src._2023;
+
+public class D1 : IAdventOfCode
+{
+    public int P1(string[] inputs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int P2(string[] inputs)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AOC/utils/interfaces/IAdventOfCode.cs
@@ -0,0 +1,7 @@
+﻿namespace src.utils.interfaces;
+
+public interface IAdventOfCode
+{
+    int P1(string[] inputs);
+    int P2(string[] inputs);
+}
