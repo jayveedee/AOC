ce75500c27469b9d1db4015e89e168334c481c3c
fo0391 <jvd@formula.fo>
2023-12-11 17:18:03 +0000

    finished day 2

--- AOC/2023/D2.cs
+++ AOC/2023/D2.cs
@@ -4,12 +4,22 @@ namespace src._2023
 {
     public class D2 : IAdventOfCode
     {
+        private readonly int MAX_RED_CUBES = 12;
+        private readonly int MAX_GREEN_CUBES = 13;
+        private readonly int MAX_BLUE_CUBES = 14;
+
         public int P1(string[] inputs)
         {
-            int maxRedCubes = 12;
-            int maxGreenCubes = 13;
-            int maxBlueCubes = 14;
+            return FindPossibleGamesAmount(inputs, false);
+        }
 
+        public int P2(string[] inputs)
+        {
+            return FindPossibleGamesAmount(inputs, true);
+        }
+
+        private int FindPossibleGamesAmount(string[] inputs, bool findMinimumViableGame)
+        {
             int possibleGames = 0;
             foreach (string line in inputs)
             {
@@ -17,45 +27,77 @@ namespace src._2023
 
                 string[] subsets = line.Split(':')[1].Split(';');
 
-                int currentRedCubes = 0;
-                int currentGreenCubes = 0;
-                int currentBlueCubes = 0;
+                bool impossibleGame = false;
+
+                int currentMaximumRedCubes = 0;
+                int currentMaximumGreenCubes = 0;
+                int currentMaximumBlueCubes = 0;
+
                 foreach (string subset in subsets)
                 {
                     string[] subsubsets = subset.Split(',');
 
-                    foreach (string subsubset in subsubsets) 
+                    int currentRedCubes = 0;
+                    int currentGreenCubes = 0;
+                    int currentBlueCubes = 0;
+
+                    foreach (string subsubset in subsubsets)
                     {
-                        int amountOfCubes = int.Parse(subsubset.Split(' ')[0]);
-                        string cubeType = subsubset.Split(' ')[1];
+                        string cleanedSet = subsubset.TrimStart(' ');
+                        int amountOfCubes = int.Parse(cleanedSet.Split(' ')[0]);
+                        string cubeType = cleanedSet.Split(' ')[1];
 
                         if (cubeType == "red")
                         {
                             currentRedCubes += amountOfCubes;
+                            if (amountOfCubes > currentMaximumRedCubes)
+                            {
+                                currentMaximumRedCubes = amountOfCubes;
+                            }
                         }
                         else if (cubeType == "green")
                         {
                             currentGreenCubes += amountOfCubes;
+                            if (amountOfCubes > currentMaximumGreenCubes)
+                            {
+                                currentMaximumGreenCubes = amountOfCubes;
+                            }
                         }
                         else
                         {
                             currentBlueCubes += amountOfCubes;
+                            if (amountOfCubes > currentMaximumBlueCubes)
+                            {
+                                currentMaximumBlueCubes = amountOfCubes;
+                            }
                         }
+
+                        if ((currentRedCubes > MAX_RED_CUBES || currentGreenCubes > MAX_GREEN_CUBES || currentBlueCubes > MAX_BLUE_CUBES) && !findMinimumViableGame)
+                        {
+                            impossibleGame = true;
+                            break;
+                        }
+                    }
+
+                    if (impossibleGame && !findMinimumViableGame)
+                    {
+                        break;
                     }
                 }
 
-                if (currentRedCubes <= maxRedCubes && currentGreenCubes <= maxGreenCubes && currentBlueCubes <= maxBlueCubes) 
+                if (!impossibleGame || findMinimumViableGame)
                 {
-                    possibleGames++;
+                    if (findMinimumViableGame)
+                    {
+                        possibleGames += (currentMaximumRedCubes * currentMaximumGreenCubes * currentMaximumBlueCubes);
+                    } else
+                    {
+                        possibleGames += gameID;
+                    }
                 }
             }
 
             return possibleGames;
-        }
-
-        public int P2(string[] inputs)
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- AOCTests/2023/D2Tests.cs
+++ AOCTests/2023/D2Tests.cs
@@ -1,28 +1,61 @@
 ﻿using AOC;
-using src.utils.interfaces;
+using NUnit.Framework;
+using src._2023;
 
-namespace AOC2021Tests._2023
+namespace test_2023
 {
-    internal class D2Tests : IAOCTests
+    internal class D2Tests : TestUtils, IAOCTests
     {
+        [Test]
         public void PartOneActual()
         {
-            throw new System.NotImplementedException();
+            // Arrange
+            var unit = new D2();
+
+            // Act
+            var actual = unit.P1(InputHandler.GetInput(2, 1));
+
+            // Assert
+            Assert.AreEqual(8, actual);
         }
 
+        [Test]
         public void PartOneExample()
         {
-            throw new System.NotImplementedException();
+            // Arrange
+            var unit = new D2();
+
+            // Act
+            var actual = unit.P1(InputHandler.GetInput(2, 1, true));
+
+            // Assert
+            Assert.AreEqual(8, actual);
         }
 
+        [Test]
         public void PartTwoActual()
         {
-            throw new System.NotImplementedException();
+            // Arrange
+            var unit = new D2();
+
+            // Act
+            var actual = unit.P2(InputHandler.GetInput(2, 2));
+
+            // Assert
+            Assert.AreEqual(70924, actual);
         }
 
+        [Test]
         public void PartTwoExample()
         {
-            throw new System.NotImplementedException();
+            // Arrange
+            var unit = new D2();
+
+            // Act
+            var actual = unit.P2(InputHandler.GetInput(2, 2, true));
+
+            // Assert
+            Assert.AreEqual(2286, actual);
         }
     }
 }
