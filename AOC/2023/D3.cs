8f1b45567d2068391e3a9eb201048b497a1f1ced
fo0391 <jvd@formula.fo>
2023-12-15 16:27:23 +0000

    started day 3

---
+++ AOC/2023/D3.cs
@@ -0,0 +1,39 @@
+﻿using src.utils.interfaces;
+
+namespace src._2023
+{
+    public class D3 : IAdventOfCode
+    {
+        public int P1(string[] inputs)
+        {
+            var result = 0;
+            var currentNumber = "";
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var currentLine = inputs[i];
+                for (int j = 0; j < currentLine.Length; j++)
+                {
+                    var currentChar = currentLine[j];
+                    if (char.IsDigit(currentChar))
+                    {
+                        currentNumber += char.ToString(currentChar);
+                        continue;
+                    } else if (currentChar == '.')
+                    {
+                        currentNumber = "";
+                        continue;
+                    }
+
+
+                }
+            }
+
+            return result;
+        }
+
+        public int P2(string[] inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AOCTests/2023/D3Tests.cs
@@ -0,0 +1,46 @@
+﻿using AOC;
+using NUnit.Framework;
+using src._2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test_2023;
+
+namespace AOC2021Tests._2023
+{
+    internal class D3Tests : TestUtils, IAOCTests
+    {
+        [Test]
+        public void PartOneActual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PartOneExample()
+        {
+            // Arrange
+            var unit = new D3();
+
+            // Act
+            var actual = unit.P1(InputHandler.GetInput(3, 1, true));
+
+            // Assert
+            Assert.AreEqual(4361, actual);
+        }
+
+        [Test]
+        public void PartTwoActual()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PartTwoExample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
